4cd73503dd447a2772a0773d33834bae6eb95e23
naza625 <nazavillafanez@gmail.com>
2026-06-14 12:49:00 -0300

    Agrego el ejercicio 7 utilizando condicionales y operadores

---
+++ js/ejercicio7.js
@@ -0,0 +1,11 @@
+const numero1 = parseInt(prompt("Ingresa el primer número:"));
+const numero2 = parseInt(prompt("Ingresa el segundo número:"));
+const numero3 = parseInt(prompt("Ingresa el tercer número:"));
+
+if (numero1 > numero2 && numero1 > numero3) {
+    document.write("El " + numero1 + " es el número más grande");
+} else if (numero2 > numero3) {
+    document.write("El " + numero2 + " es el número más grande");
+} else {
+    document.write("El " + numero3 + " es el número más grande");
+}
